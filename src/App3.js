0b607368472abb340be05d6f8a4fa03a9d51cf54
hyeri <lilyantina@naver.com>
2019-09-21 16:34:53 +0900

    0921

--- src/App3.js
+++ src/App3.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
-/* // 
+import $ from 'jquery'
+/*
     class A
     {
         public A(String name, int age, String addr) {
@@ -12,49 +13,92 @@ import React, {Component} from 'react'
 //단순한 1개기능 출력용이면 함수형이 좋다
 class App3 extends Component{
 
+    //변수 선언하거나... 이벤트를...
     constructor(props) {
         super(props); // Component의 생성자 호출
         console.log("constuctor Call...")
+        //constructor 안에 선언된 변수(전역변수)는 this.으로 접근한다.
     }
 
+    //파일을 읽거나... 서버에서 전달해주는 값을 받거나...
     componentWillMount() {
         console.log("componentWillMount() Call...");
     }
 
+    //메모리에 올리는 중...
     render() {
         console.log("render() Call...")
+        /* map함수 표현방식 3가지
+            const html = this.props.music.map((m)=>());
+            const html = this.props.music.map((m)=>{
+                return ();
+            });
+            const html = this.props.music.map(fuction(m) {
+                return ();
+            })
+            const html = this.props.music.forEach(fuction(m) {
+                return ();
+            })
+
+        */
         const html = this.props.music.map((m, idx)=>(
             <tr key={idx}>
-                <th>{m.rank}</th>
-                <th><img src={m.poster} width={"30"} height={"30"}/></th>
-                <th>{m.title}</th>
-                <th>{m.singer}</th>
+                <td>{m.rank}</td>
+                <td><img src={m.poster} width={"30"} height={"30"} alt={"앨범표지"}/></td>
+                <td>{m.title}</td>
+                <td>{m.singer}</td>
             </tr>
         ));
+
+        //return문에서는 html만!! 조작은 이 밖에서!!
+        //화면 UI 라이브러리
+        /*
+            MVC
+            ==========
+            Model
+            View ==========> React
+            Controller
+        */
         return(
             <div>
                     {/*이름 : {this.props.name} <br/>*/}
                     {/*나이 : {this.props.age} <br/>*/}
                     {/*주소 : {this.props.addr} <br/>*/}
-                    <table className={"table table-hover"}>
+                    <table className={"table"}>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <input type={"text"} id={"keyword"} size={"20"} className={"input-sm"} />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table className={"table table-hover"} id={"user-table"}>
                         <thead>
-                        <tr className={"danger"}>
-                            <th>순위</th>
-                            <th>제목</th>
-                            <th>노래명</th>
-                            <th>가수명</th>
-                        </tr>
+                            <tr className={"danger"}>
+                                <th>순위</th>
+                                <th></th>
+                                <th>노래명</th>
+                                <th>가수명</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        {html}
+                            {html}
                         </tbody>
                     </table>
             </div>
         );
     }
 
+    //화면 출력 시점에... 따라서 외부 연동은 여기서...
     componentDidMount() { //onLoad기능...sciprt 및 Jquery 연동은 이곳에서...
         console.log("componentDidMount Call...")
+        $('#keyword').keyup(function () {
+            const k = $(this).val();
+            $('#user-table>tbody>tr').hide();
+            const temp = $(`#user-table>tbody>tr>td:nth-child(4n+3):contains("${k}")`);
+            $(temp).parent().show();
+        });
     }
 
 }
